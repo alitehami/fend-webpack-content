6238a7e39e329a0260f24d13edf4ac52cbe7e4b9
ali-tehami <ali.tehami@yahoo.com>
2020-06-26 03:14:54 +1000

    added babel to the project

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,6 @@
-alert("Hello!! EXIT");
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+
+console.log(checkForName);
+
+alert("I EXIST");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const path = require("path");
 const webpack = require("webpack");
+const { timeStamp } = require("console");
 module.exports = {
-    entry:'./src/client/index.js'
+  entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
 };
